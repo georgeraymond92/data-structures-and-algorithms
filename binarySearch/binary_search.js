a34a72416d3494509ba5fa2ae6c74077268485ea
george <geomarray@gmail.com>
2018-12-20 08:31:27 -0800

    pull request for submit

--- binarySearch/binary_search.js
+++ binarySearch/binary_search.js
@@ -1,24 +1,18 @@
-const test1 = [2,4,6,8,10];
-
 
 const binarySearch = (arr, el) => {
     let center = Math.ceil(arr.length/2 -1);
 
     if(arr[center] >=el) {
         for(let i = center; i > -1; i--){
-            // console.log(i);
             if(arr[i] === el) {
                 return i;
             }
         }
         return -1;
     }else if(arr[center] <= el) {
-        // console.log(i);
+
         console.log('hello');
         for(let i = center; i < arr.length; i++){
-            // console.log(i);
-            console.log('hello');
-            // if(i < 10){console.log(i)};
             if(arr[i] === el){
                 console.log('hello');
                 return i;
@@ -28,6 +22,4 @@ const binarySearch = (arr, el) => {
         return -1;
     }
 }
-
-console.log(binarySearch(test1, 4));
-console.log(binarySearch(test1, 10));
+module.exports = binarySearch;
