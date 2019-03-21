181e3447b441986ae4e1c1a2368bafb142409804
george <geomarray@gmail.com>
2019-03-20 17:49:33 -0700

    graph class with methods moving into traversals

--- graphs/graph/graph.js
+++ graphs/graph/graph.js
@@ -11,11 +11,11 @@ class Graph {
 
   addEdge(vertexOne, vertexTwo) {
     this.adjacencyList[vertexOne].push(vertexTwo);
-    this.adjacencyList[vertexTwo].pusht(vertexOne);
+    this.adjacencyList[vertexTwo].push(vertexOne);
   }
 
-  removeEdge(vetexOne,vertexTwo) {
-    for(let i = 0; i < adjacencyList[vetexOne].length; i++) {
+  removeEdge(vertexOne,vertexTwo) {
+    for(let i = 0; i < this.adjacencyList[vertexOne].length; i++) {
       this.adjacencyList[vertexOne] = this.adjacencyList[vertexOne].filter(
         v => v !== vertexTwo
       )
@@ -25,4 +25,35 @@ class Graph {
     }
 
   }
+
+  removeVertex(vertex) {
+    while(this.adjacencyList[vertex].length){
+      const adjacentVertex = this.adjacencyList[vertex].pop();
+      this.removeEdge(vertex, adjacentVertex);
+    }
+    delete this.adjacencyList[vertex];
+    
+  }
 }
+
+const g = new Graph();
+
+g.addVertex('Seattle');
+g.addVertex('Los Angeles');
+g.addVertex('New York');
+g.addVertex('San Francisco');
+g.addEdge('Seattle','Los Angeles');
+g.addEdge('Seattle','New York');
+g.addEdge('Seattle','San Francisco');
+g.addEdge('New York','Los Angeles');
+g.addEdge('New York','San Francisco');
+g.addEdge('Los Angeles','San Francisco');
+g.removeEdge('San Francisco', 'New York');
+g.removeVertex('Los Angeles');
+
+
+console.log(g.adjacencyList);
+
+
+
+module.exports = Graph;
